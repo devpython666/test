45b90b9fe2d858b02ce61c3a5fb3d98a8c4da966
devpython666 <63408694+devpython666@users.noreply.github.com>
2020-06-02 02:36:05 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.slider').slick({
+    nextArrow: '<button class="slick-arrow slick-next"><img src="img/next.png" alt="next arrow"></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="img/prev.png" alt="prev arrow"></button>',
+    
+  });
+});
